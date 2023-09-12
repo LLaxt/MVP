b49bcc749f5f7e1360f9aaed6dc00fef37085303
LLaxt <laurenlaxton1@gmail.com>
2023-09-12 01:39:33 -0700

    add font

--- RansomNotesApp/app/_layout.jsx
+++ RansomNotesApp/app/_layout.jsx
@@ -15,7 +15,7 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    CourierPrime: require('../assets/fonts/CourierPrime-Regular.ttf'),
     ...FontAwesome.font,
   });
 
